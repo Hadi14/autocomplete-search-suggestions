21a2d6084e58d56b9778794d7cf2002bb7ca2406
Hadi14 <asusn43@yahoo.com>
2022-10-29 21:37:48 +0330

    ok

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const input = document.querySelector('input');
+const si = document.querySelector('.search-input');
+const a = document.querySelector('.autocom-box');
+let li;
+
+let s = "";
+// let suggestions = [
+//     "Channel",
+//     "CodingLab",
+//     "CodingNepal",
+//     "YouTube",
+//     "YouTuber",
+//     "YouTube Channel",
+//     "Blogger",
+//     "Bollywood",
+//     "Vlogger",
+//     "Vechiles",
+//     "Facebook",
+//     "Freelancer",
+//     "Facebook Page",
+//     "Designer",
+//     "Developer",
+//     "Web Designer",
+//     "Web Developer",
+//     "Login Form in HTML & CSS",
+//     "How to learn HTML & CSS",
+//     "How to learn JavaScript",
+//     "How to become Freelancer",
+//     "How to become Web Designer",
+//     "How to start Gaming Channel",
+//     "How to start YouTube Channel",
+//     "What does HTML stands for?",
+//     "What does CSS stands for?",
+// ];
+input.addEventListener('input', () => {
+    a.innerHTML = "";
+    s = input.value;
+    suggestions.forEach((str) => {
+        if (str.search(s) >= 0 && s != "") {
+            // console.log(str + ": " + str.search(s));
+            // console.log(a);
+            a.insertAdjacentHTML('beforeend', "<li>" + str + "</li>")
+        }
+    })
+    si.classList.add("active");
+    li = document.querySelectorAll('.autocom-box li');
+    li.forEach((item) => {
+        item.addEventListener('click', () => {
+            input.value = item.innerHTML;
+            a.innerHTML = "";
+            // a.style.opacity = "0";
+        })
+    })
+    // console.log(li)
+})
